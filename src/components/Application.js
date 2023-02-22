f64a387856bd0f9131b30a97cee0338194f01ab9
Graham Fujisawa <sfrrki@gmail.com>
2023-02-21 18:50:09 -0800

    add days and test stories

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,21 @@
+const days = [{
+  id: 1,
+  name: "Monday",
+  spots: 2,
+}, {
+  id: 2,
+  name: "Tuesday",
+  spots: 5,
+}, {
+  id: 3,
+  name: "Wednesday",
+  spots: 0,
+},];
+
 import React from "react";
 
 import "components/Application.scss";
+
 
 export default function Application(props) {
   return (
