50f63d2caa183455ca8c6d34e6e98a990efc412f
Graham Fujisawa <sfrrki@gmail.com>
2023-02-21 17:51:33 -0800

    add test and variable text

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,15 +9,24 @@ export default function DayListItem(props) {
   const setDay = props.setDay;
   const DLIClass = classNames(
     "day-list__item", {
-      "day-list__item--selected": props.selected === true,
-      "day-list__item--full": props.spots === 0
+    "day-list__item--selected": props.selected === true,
+    "day-list__item--full": props.spots === 0
+  });
+  const formatSpots = (spots) => {
+    console.log("current number of spots:", spots);
+    if (spots === 0) {
+      return "no spots"
     }
-  )
+    if (spots === 1) {
+      return "1 spot"
+    }
+    return `${spots} spots`;
+  };
 
   return (
     <li className={DLIClass} onClick={() => setDay(name)}>
-      <h2 className="text--regular setDay">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h2 className="text--regular setDay">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
