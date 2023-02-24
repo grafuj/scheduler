d23f56ba1df4402565e029a2b168f33b93c95a71
Graham Fujisawa <sfrrki@gmail.com>
2023-02-23 16:16:24 -0800

    add appt display view of a few

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,13 +1,19 @@
 import React from "react";
+import Empty from "./Empty";
+import Header from "./Header";
+import Show from "./Show";
 
 // import Appointment from "components/Appointment";
 import "./styles.scss";
-// import Header from "Header.js";
+
 
 export default function Appointment(props) {
-
+  const { interview, time } = props;
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={time} />
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer.name} /> : <Empty />}
+    </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -21,6 +21,7 @@ import Confirm from "components/Appointments/Confirm";
 import Status from "components/Appointments/Status";
 import Error from "components/Appointments/Error";
 import Form from "components/Appointments/Form";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -174,4 +175,20 @@ storiesOf("Appointment", module)
   onSave={action("onSave")}
   onCancel={action("onCancel")}
   />)
-
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  
